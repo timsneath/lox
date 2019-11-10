d75ce2fc9e5348888699dd8fd488a35a4eff6047
Tim Sneath <timsneath@google.com>
2019-11-10 20:00:52 +0100

    Initial shell

---
+++ lox.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:dart_console/dart_console.dart';
+
+bool hadError = false;
+
+final console = Console();
+
+void main(List<String> args) {
+  if (args.length > 1) {
+    console.writeLine('Usage lox [script]');
+    exit(64);
+  } else if (args.length == 1) {
+    runFile(args[0]);
+  } else {
+    runPrompt();
+  }
+}
+
+void runFile(String path) {
+  final uri = Uri(path: path);
+  final file = File.fromUri(uri).readAsStringSync();
+  run(file);
+
+  if (hadError) exit(65);
+}
+
+void runPrompt() {
+  for (;;) {
+    console.write('> ');
+    run(console.readLine());
+    hadError = false;
+  }
+}
+
+void run(String source) {
+  final scanner = Scanner(source);
+  List<Token> tokens = scanner.scanTokens();
+
+  for (final token in tokens) {
+    console.writeLine(token);
+  }
+}
+
+void error(int line, String message) {
+  report(line, "", message);
+}
+
+void report(int line, String where, String message) {
+  stderr.write('[line $line] Error$where: $message');
+  hadError = true;
+}
